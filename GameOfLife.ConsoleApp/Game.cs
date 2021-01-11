eb7d34a729b4cd386de6892a1ab707295eaf7ba1
oatsoda <andrew+github@milne.tel>
2021-01-11 15:31:18 +0000

    Initial commit

---
+++ GameOfLife.ConsoleApp/Game.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GameOfLife.ConsoleApp
+{
+    public class Game
+    {
+        private readonly bool m_WrapGrid;
+
+        public bool[,] Grid { get; }
+
+        public int Cols { get; }
+        public int Rows { get; }
+
+        public Game(int cols, int rows, int seedLiveSquares, bool wrapGrid)
+        {
+            Cols = cols;
+            Rows = rows;
+            m_WrapGrid = wrapGrid;
+            Grid = new bool[cols,rows];
+
+            var rdm = new Random();
+
+            for (int i = 0; i < seedLiveSquares; i++)
+            {
+                int x, y;
+                do
+                {
+                    x = rdm.Next(0, cols);
+                    y = rdm.Next(0, rows);
+                }
+                while (Grid[x,y]);
+
+                Grid[x,y] = true;
+            }
+        }
+
+        public List<(int x, int y, bool alive)> Tick()
+        {
+            // Calc changes
+            var changes = new List<(int x, int y, bool alive)>();
+            
+            foreach (var (x, y) in GetGridSquares())
+            {
+                var n = LiveNeighbours(x,y);
+
+                /*
+                Any live cell with fewer than two live neighbours dies, as if by underpopulation.
+                Any live cell with two or three live neighbours lives on to the next generation.
+                Any live cell with more than three live neighbours dies, as if by overpopulation.
+                Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
+                */
+                var alive = Grid[x,y];
+
+                if (!alive)
+                {
+                    if (n == 3)
+                        changes.Add((x,y,true));
+                    continue;
+                }
+
+                if (n == 2 || n == 3)
+                    continue;
+
+                changes.Add((x,y,false));
+            }
+
+            // Apply changes
+            foreach (var change in changes)
+                Grid[change.x, change.y] = change.alive;
+
+            return changes;
+        }
+
+        public IEnumerable<(int x, int y)> GetGridSquares()
+        {
+            for (int x = 0; x < Cols; x++)
+            {
+                for (int y = 0; y < Rows; y++)
+                {
+                    yield return (x, y);
+                }
+            }
+        }
+
+        public bool IsDeserted => GetGridSquares().All(square => !Grid[square.x, square.y]);
+
+        private int LiveNeighbours(int x, int y)
+        {
+            var t = 0;
+
+            for (int xN = x-1; xN <= x+1; xN++)
+            {
+                if (xN < 0 || xN >= Cols)
+                    continue;
+
+                for (int yN = y-1; yN <= y+1; yN++)
+                {
+                    if (yN < 0 || yN >= Rows || (xN == x && yN == y))
+                        continue;
+
+                    if (Grid[xN,yN])
+                        t++;
+                }
+
+            }
+
+            return t;
+        }
+    }
+}
---
+++ GameOfLife.ConsoleApp/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Threading;
+
+namespace GameOfLife.ConsoleApp
+{
+    class Program
+    {      
+        private const int GRID_COLS = 40;
+        private const int GRID_ROWS = 30;
+        private const int SEED_LIVE_SQUARES = 400;
+
+        const int _MAX_TICKS = 80;
+        const int _TICK_INTERVAL = 400;
+
+        static (int x, int y) s_CursorPos;
+
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.CursorVisible = false;
+
+            var game = new Game(GRID_COLS, GRID_ROWS, SEED_LIVE_SQUARES, false);
+            DrawGame(game);
+
+            s_CursorPos = (0, game.Rows + 2);
+            ResetCursor();
+            Console.WindowHeight = s_CursorPos.y + 10;
+
+            Console.ReadLine();
+
+            for (int t = 1; t <= _MAX_TICKS; t++)
+            {
+                var changes = game.Tick();
+                //Console.Beep(1100, 200);
+
+                foreach (var change in changes)
+                    DrawSquare(change);
+
+                ResetCursor();
+                Console.WriteLine(t.ToString().PadRight(Console.WindowWidth));
+                if (game.IsDeserted)
+                {
+                    Console.WriteLine($"DEAD AFTER {t} TICKS :(");
+                    return;
+                }
+
+                Thread.Sleep(_TICK_INTERVAL);
+            }
+                    
+            Console.WriteLine($"SURVIVED {_MAX_TICKS} TICKS.");
+        }
+
+        static void DrawGame(Game game)
+        {
+            foreach ((int x, int y) square in game.GetGridSquares())
+            {
+                if (!game.Grid[square.x,square.y])
+                    continue;
+
+                DrawSquare((square.x,square.y,true));
+            }            
+        }
+
+        static void DrawSquare((int x, int y, bool alive) square)
+        {
+            Console.SetCursorPosition(square.x * 2, square.y);
+            Console.Write(square.alive ? "▒▒" : "  ");
+        }
+
+        static void ResetCursor() => Console.SetCursorPosition(s_CursorPos.x, s_CursorPos.y);
+    }
+}
